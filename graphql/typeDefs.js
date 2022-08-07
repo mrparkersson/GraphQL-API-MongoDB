705e2559bcfa7ccf3c3f3d5a72b06d0818e4608f
mrparkersson <amoparkerprince78@gmail.com>
2022-08-07 08:49:06 +0000

    Create query for geting all memo and getting by id

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -13,19 +13,29 @@ const typeDefs = gql`
     password: String
   }
 
+  type Memo {
+    id: ID
+    title: string
+    description: sting
+  }
+
   type Query {
     hello: String
     getAllPost: [Post!]!
     getPostById(id: ID): Post
     getUser: [User!]!
     getUserById(id: ID): User
+    getAllMemo: [Memo!]!
+    getMemoById(id: ID): Memo
   }
 
   type Mutation {
     createPost(title: String, description: String): Post
-    deletePost(id: ID): Post
+    deletePost(id: ID): String
     createNewUser(username: String, password: String): User
-    deleteUserById(id: ID): User
+    deleteUserById(id: ID): String
+    createNewMemo(title: string, description: string): Memo
+    deleteMemoById(id: ID): string
   }
 `;
 
